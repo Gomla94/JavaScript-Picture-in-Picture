947e019c903317b1e0f21229992643fa377cb220
Ahmed <ahmedgamal1994@outlook.com>
2021-01-04 14:44:50 +0200

    change selectMediaFun

--- script.js
+++ script.js
@@ -2,33 +2,36 @@ const video = document.getElementById('video');
 const shareButton = document.getElementById('share-button');
 const startButton = document.getElementById('start-button');
 const loader = document.getElementById('loader');
-startButton.disabled = true;
+// startButton.disabled = true;
 
-async function selectMedia() {
-    try {
-        const mediaStreamDevice = await navigator.mediaDevices.getDisplayMedia();
-        video.srcObject = mediaStreamDevice;
-        video.onloadedmetadata = async () => {
-            await video.play();
-            startButton.disabled = false;
-        }
-    } catch (error) {
-        console.log(error)
-    }
-}
+video.setAttribute('src', 'Screen Recording 2020-11-18 at 10.36.21 AM.mov');
 
-// button.addEventListener('click', async () => {
-//     button.disabled = true;
-//     await video.requestPictureInPicture();
-//     button.disabled = false;
-// });
+// async function selectMedia() {
+//     try {
+//         const mediaStreamDevice = await navigator.mediaDevices.getDisplayMedia();
+//         video.srcObject = mediaStreamDevice;
+//         video.onloadedmetadata = async () => {
+//             await video.play();
+//             startButton.disabled = false;
+//         }
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
-shareButton.addEventListener('click', selectMedia);
+// // button.addEventListener('click', async () => {
+// //     button.disabled = true;
+// //     await video.requestPictureInPicture();
+// //     button.disabled = false;
+// // });
+
+// shareButton.addEventListener('click', selectMedia);
 startButton.addEventListener('click', startPictureInPicture);
 
 
 async function startPictureInPicture() {
     startButton.disabled = true;
+    video.hidden = true;
     video.requestPictureInPicture();
 }
 // selectMedia();
