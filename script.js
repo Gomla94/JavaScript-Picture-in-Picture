650d828026a7ce5eb31c42e48f146b1086dcdba3
Ahmed <ahmedgamal1994@outlook.com>
2021-01-04 11:36:24 +0200

    change selectMediaFun

--- script.js
+++ script.js
@@ -5,18 +5,30 @@ async function selectMedia() {
     try {
         const mediaStreamDevice = await navigator.mediaDevices.getDisplayMedia();
         video.srcObject = mediaStreamDevice;
-        video.onloadedmetadata = async () => {
-            await video.play();
-        }
+        // video.onloadedmetadata = async () => {
+        //     await video.play();
+        // }
+        video.onloadedmetadata = startPictureInPicture();
     } catch (error) {
         console.log(error)
     }
 }
 
-button.addEventListener('click', async () => {
-    button.disabled = true;
-    await video.requestPictureInPicture();
-    button.disabled = false;
-});
+// button.addEventListener('click', async () => {
+//     button.disabled = true;
+//     await video.requestPictureInPicture();
+//     button.disabled = false;
+// });
 
-selectMedia();
+button.addEventListener('click', selectMedia);
+
+
+async function startPictureInPicture() {
+    await video.play();
+    if (video.play()) {
+        video.requestPictureInPicture();
+    } else {
+        startPictureInPicture();
+    }
+}
+// selectMedia();
